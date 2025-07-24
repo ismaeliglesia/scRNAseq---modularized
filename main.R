cd093389f9db5bd84697547fbb10b875bbceef8c
ismaeliglesia <123124878+ismaeliglesia@users.noreply.github.com>
2025-07-24 15:15:47 -0500

    Update main.R
    Updated to fit with new modules.R

--- main.R
+++ main.R
@@ -22,6 +22,8 @@ assign(project_name, load_RNA_data("./count/filtered_feature_bc_matrix.h5",
                                    filter_singlets = TRUE, 
                                    aggr_dir = "./count/aggregation.csv", 
                                    metadata_file = NULL,
+                                   sample_column_name = NULL,
+                                   hash_column_name = NULL,
                                    contig_list = "./count/filtered_contig_annotations.csv"))
 
 assign(project_name, qc_filtering(seurat_obj = get(project_name), mito_percent_qc = 10, min_genes_qc = 500, max_genes_qc = 8000))
